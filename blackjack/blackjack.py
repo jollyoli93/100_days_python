0ed10967a310aadb1c6265e99b1aacaacd7b4033
jollyoli93 <jackoliver1993@yahoo.co.uk>
2024-02-15 22:20:58 +0000

    player logic functional

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,32 +1,65 @@
 from random import choice
-from game_logic import add_cards
+from game_logic import add_cards, twist, win_conditon, dealer_view
 
 cards = {"Ace":11, "Two":2,"Three":3, "Four":4, "Five":5, "Six":6, "Seven":7, "Eight":8, "Nine":9, "Ten":10, "Jack":10, "Queen":10, "King":10}
+bust = False
+player_pot = 1000
+
+#insert white loop
+
 #place bet
 bet = input("Place your bets please ")
 print("£" + bet)
 
-#dealer deals
-players_cards_list = list(cards.keys())
-players_cards = [choice(players_cards_list), choice(players_cards_list)]
+while not bust:
+    #dealer deals
+    players_cards_list = list(cards.keys())
+    players_cards = [choice(players_cards_list), choice(players_cards_list)]
 
-dealers_cards_list = list(cards.keys())
-dealers_cards = [choice(players_cards_list), choice(players_cards_list)]
+    dealers_cards_list = list(cards.keys())
+    dealers_cards = [choice(players_cards_list), choice(players_cards_list)]
 
-#Show Cards
-print(players_cards, dealers_cards[0])
+    #Show Cards
+    print(f"Players: {players_cards}")
+    print( f"Dealers: {dealers_cards[0]}")
 
-#check if cards under 17, if so must hit, elif < 17 hit or stick
-players_total = add_cards(players_cards, cards)
-print(players_total)
+    #check if cards under 17, if so must hit, elif < 17 hit or stick
+    players_total = add_cards(players_cards, cards)
 
-if players_total < 17:
-    twist()
-if players_total > 17:
-    response = input("Stick or Twist? ")
-else:
-    stick()
+    print(players_total)
 
-#check if >21 then bust
-#if <21, hit or twist?
-#if >21 bust, elif dealer == player, draw. elif player > dealer, win. else, lose.\\wsl.localhost\Ubuntu\home\jollyoli93\python_projects
+#players turn
+    while players_total < 17:
+        print("Player must twist")
+        players_cards = twist(players_cards, cards)
+        players_total = add_cards(players_cards, cards)
+
+    if players_total > 21:
+        bust = True
+        break
+
+    while True:
+        print(f"Players: {players_cards}")
+        print( f"Dealers: {dealers_cards[0]}")
+        response = input("Stick or Twist? ")
+
+        if response.lower() == "twist":
+            print("Player twists")
+            players_cards = twist(players_cards, cards)
+            players_total = add_cards(players_cards, cards)
+            if players_total > 21:
+                print(f"{players_total}, player loses")
+                bust = True
+                break
+        elif response.lower() == "stick":
+            print("Player sticks")
+            bust = True
+            break
+        else:
+            print("Wrong answer, try again")
+
+
+
+
+#dealers turn
+    
--- blackjack/game_logic.py
+++ blackjack/game_logic.py
@@ -1,11 +1,36 @@
+from random import choice
+
 def add_cards(card_list, cards):
     total = 0
     for card in card_list:
         total += cards[card]
     return total
 
-def twist():
-    pass
+def dealer_view(player, dealer):
+    for i, card in enumerate(player):
+        print(f"player has {card}")
+    player_total = add_cards(player)
 
-def stick():
-    pass
+    for i, card in enumerate(player):
+        print(f"Dealer has {dealer[i+1]}")
+
+def twist(deck_input, card_input):
+    hand = deck_input
+    new_card = list(card_input.keys())
+    hand = hand + [choice(new_card)]
+    return hand
+
+def win_conditon(player, dealer):
+    if player == dealer:
+        winner = "draw"
+        return winner
+    
+    elif player > dealer:
+        winner = player
+        return winner
+    
+    elif player < dealer:
+        winner = dealer
+        return winner
+     
+   
