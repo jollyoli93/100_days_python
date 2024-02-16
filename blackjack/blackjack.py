40e6da5e8d743b0a168b26dc283170ea65a34e20
jollyoli93 <jackoliver1993@yahoo.co.uk>
2024-02-16 09:08:59 +0000

    dealer logic added

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -5,13 +5,11 @@ cards = {"Ace":11, "Two":2,"Three":3, "Four":4, "Five":5, "Six":6, "Seven":7, "E
 bust = False
 player_pot = 1000
 
-#insert white loop
-
 #place bet
-bet = input("Place your bets please ")
-print("£" + bet)
-
 while not bust:
+    bet = input("Place your bets please ")
+    print("£" + bet)
+
     #dealer deals
     players_cards_list = list(cards.keys())
     players_cards = [choice(players_cards_list), choice(players_cards_list)]
@@ -20,10 +18,9 @@ while not bust:
     dealers_cards = [choice(players_cards_list), choice(players_cards_list)]
 
     #Show Cards
-    print(f"Players: {players_cards}")
-    print( f"Dealers: {dealers_cards[0]}")
+    print(f"Player: {players_cards}")
+    print( f"Dealer: {dealers_cards[0]}")
 
-    #check if cards under 17, if so must hit, elif < 17 hit or stick
     players_total = add_cards(players_cards, cards)
 
     print(players_total)
@@ -51,6 +48,9 @@ while not bust:
                 print(f"{players_total}, player loses")
                 bust = True
                 break
+            elif players_total == 21:
+                print("21!")
+                break
         elif response.lower() == "stick":
             print("Player sticks")
             bust = True
@@ -58,8 +58,15 @@ while not bust:
         else:
             print("Wrong answer, try again")
 
-
-
-
 #dealers turn
-    
+            
+    dealers_total = add_cards(dealers_cards, cards)
+            
+    while dealers_total < 17:
+        dealers_cards = twist(dealers_cards, cards)
+        dealers_total = add_cards(dealers_cards, cards)
+        if dealers_total > 21:
+            bust = True
+        else:
+            print("Dealer sticks")
+            print(dealers_cards)
