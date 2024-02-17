f8a5b56ba8a7e8b383f53182aaa1ae6fcc438b51
jollyoli93 <jackoliver1993@yahoo.co.uk>
2024-02-17 02:14:48 +0000

    game func and UI improvements

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,110 +1,112 @@
 from random import choice
 from game_logic import add_cards, twist, win_conditon, print_player_cards, print_dealer_cards
 
-cards = {"Ace":11, "Two":2,"Three":3, "Four":4, "Five":5, "Six":6, "Seven":7, "Eight":8, "Nine":9, "Ten":10, "Jack":10, "Queen":10, "King":10}
+def play_game():
+    cards = {"Ace":11, "Two":2,"Three":3, "Four":4, "Five":5, "Six":6, "Seven":7, "Eight":8, "Nine":9, "Ten":10, "Jack":10, "Queen":10, "King":10}
+    player_pot = 1000
+    game_over = False
 
-player_pot = 1000
-game_over = False
-#place bet
-while not game_over:
-    start_game = input("Press enter to play or no to quit: ")
-    print(start_game)
+    while not game_over:
+        start_game = input("Press enter to play or no to quit: ")
+        print(start_game)
 
-    if player_pot == 0:
-        game_over = True
-        break
-    elif start_game.lower() =='no':
-        game_over = True
-        break
+        if player_pot <= 0:
+            game_over = True
+            return "Out of money, go home!"
+        elif start_game.lower() =='no':
+            game_over = True
+            return "End of game"
 
-    bust = False
-    print("Place your bets please:")
-    bet = input("£")
-    player_pot = player_pot - int(bet)
-    print("\
-            ")    
-
-    #dealer deals
-    players_cards_list = list(cards.keys())
-    players_cards = [choice(players_cards_list), choice(players_cards_list)]
-
-    dealers_cards_list = list(cards.keys())
-    dealers_cards = [choice(players_cards_list), choice(players_cards_list)]
-
-    #Show Cards
-    print("Dealer deals the cards")
-    print("\
-            ")    
-    
-    print_player_cards(players_cards)
-    print_dealer_cards(dealers_cards)
-    players_total = add_cards(players_cards, cards)
-
-    #players turn
-    while players_total < 17:
-        print(input("Player must twist, press to continue"))
-        players_cards = twist(players_cards, cards)
-        players_total = add_cards(players_cards, cards)
-        print_player_cards(players_cards)
-
-    if players_total > 21:
-        print("Player bust!")
-        bust = True
-
-    while not bust:
-        response = input("Stick or Twist? ")
+        bust = False
+        print("Place your bets please:")
+        bet = input("£")
+        player_pot = player_pot - int(bet)
         print("\
-              ")
+                ")    
 
-        if response.lower() == "twist":
-            print("Player twists")
-            print("\
-                ")            
+        #dealer deals
+        players_cards_list = list(cards.keys())
+        players_cards = [choice(players_cards_list), choice(players_cards_list)]
+
+        dealers_cards_list = list(cards.keys())
+        dealers_cards = [choice(players_cards_list), choice(players_cards_list)]
+
+        #Show Cards
+        print("Dealer deals the cards")
+        print("\
+                ")    
+        
+        print_player_cards(players_cards)
+        print_dealer_cards(dealers_cards)
+        players_total = add_cards(players_cards, cards)
+
+        #players turn
+        while players_total < 17:
+            print(input("Player must twist, press to continue"))
             players_cards = twist(players_cards, cards)
             players_total = add_cards(players_cards, cards)
             print_player_cards(players_cards)
 
-            if players_total > 21:
-                print(f"{players_total}, player loses")
-                bust = True
-                break
-            elif players_total == 21:
-                print("21!")
-                break
-
-        elif response.lower() == "stick":
-            print("Player sticks")
-            print("\
-                ")            
-            break
-        else:
-            print("Wrong answer, try again")
-            print("\
-                ")            
-
-    #dealers turn
-    
-    if not bust:
-        print("Dealers turn")
-        print("\
-              ")
-        dealers_total = add_cards(dealers_cards, cards)
-        print_dealer_cards(dealers_cards)
-                
-        while dealers_total < 17:
-            dealers_cards = twist(dealers_cards, cards)
-            dealers_total = add_cards(dealers_cards, cards)
-            print_dealer_cards(dealers_cards)
-            print("\
-                ")            
-
-        if dealers_total > 21:
+        if players_total > 21:
+            print("Player bust!")
             bust = True
-        else:
-            print("Dealer sticks")
+
+        while not bust:
+            response = input("Stick or Twist? ")
             print("\
                 ")
-    #check winners
-    if not bust:
-        winner = win_conditon(players_total, dealers_total)
-        print(winner)
+
+            if response.lower() == "twist":
+                print("Player twists")
+                print("\
+                    ")            
+                players_cards = twist(players_cards, cards)
+                players_total = add_cards(players_cards, cards)
+                print_player_cards(players_cards)
+
+                if players_total > 21:
+                    print(f"{players_total}, player loses")
+                    bust = True
+                    break
+                elif players_total == 21:
+                    print("21!")
+                    break
+
+            elif response.lower() == "stick":
+                print("Player sticks")
+                print("\
+                    ")            
+                break
+            else:
+                print("Wrong answer, try again")
+                print("\
+                    ")            
+
+        #dealers turn
+        
+        if not bust:
+            print("Dealers turn")
+            print("\
+                ")
+            dealers_total = add_cards(dealers_cards, cards)
+            print_dealer_cards(dealers_cards)
+                    
+            while dealers_total < 17:
+                dealers_cards = twist(dealers_cards, cards)
+                dealers_total = add_cards(dealers_cards, cards)
+                print_dealer_cards(dealers_cards)
+                print("\
+                    ")            
+
+            if dealers_total > 21:
+                bust = True
+            else:
+                print("Dealer sticks")
+                print("\
+                    ")
+        #check winners
+        if not bust:
+            winner = win_conditon(players_total, dealers_total)
+            print(winner)
+
+print(play_game())
