a31f255c8ed945788f183f3ff67ed76b5119401e
jollyoli93 <jackoliver1993@yahoo.co.uk>
2024-02-19 11:26:40 +0000

    Refactored to functions

--- high_low/high_low.py
+++ high_low/high_low.py
@@ -1,38 +1,61 @@
 from random import randint
-numbers_to_100 = randint(0, 101)
 
-def game(number):
-    game_over = False
+def set_difficulty():
+    EASY_LIFE = 10
+    HARD_LIVE = 5
 
-    while not game_over:
-        response = input("Do you want to play Easy or Hard? or press no to quit ")
+    response = input("Select difficulty: Press enter for Easy or type Hard --- press no to quit ")
 
-        if response.lower() == "no":
-            game_over = True
-            break
-        elif response.lower() == "hard":
-            lives = 5
+    if response.lower() == "no":
+        return True
+    elif response.lower() == "hard":
+        return HARD_LIVE
+    else:
+        return EASY_LIFE
+
+def user_guess(number, lives):
+        print("\n")
+        guess_str = input("guess my number? ")
+        guess = int(guess_str)
+
+        if lives == 0:
+            print("Game over")
+            return 
+        elif guess == number:
+            print("Correct!")
+            return True
+        elif guess > number:
+            print("Too high, guess again")
+            return 
+        elif guess < number:
+            print("Too low, guess again")
+            return 
         else:
-            lives = 10
+            print("Invalid number")
+            return
 
 
-        while lives > 0:
-            print(f"{lives} Lives")
-            print(number)
+def game():
+    numbers_to_100 = randint(0, 101)
 
-            guess_str = input("guess my number? ")
-            guess = int(guess_str)
+    lives = set_difficulty()
 
-            if guess == number:
-                print("Correct!")
-                break
-            elif guess > number:
-                print("Too high, guess again")
-                lives -= 1
-            elif guess < number:
-                print("Too low, guess again")
-                lives -= 1
-            else:
-                print("Invalid number")
+    print(numbers_to_100)
+    print(f"You have {lives} lives remaining")
+    
+    while lives > 0:
+        play = user_guess(numbers_to_100, lives)
 
-game(numbers_to_100)
+        if play == True:
+            break
+        elif lives == 0:
+            print("Game over")
+            break
+        else:
+            lives -= 1
+            print(f"You have {lives} lives remaining")
+    
+    return 
+
+
+game()
